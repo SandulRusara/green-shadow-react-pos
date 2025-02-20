b261d4f21e9ef0fee64e0850760d299f8f56c206
SandulRusara <sandulrusara8@gmail.com>
2025-02-20 15:11:41 +0530

    update some files

--- src/pages/Crop.tsx
+++ src/pages/Crop.tsx
@@ -1,10 +1,9 @@
-import { Cards } from "../components/Cards";
+
 import { Addbutton } from "../components/Addbutton";
 
 import { Modal } from "../components/Modal";
 import { Savebutton } from "../components/Savebutton";
 import { Updatebutton } from "../components/Updatebutton";
-import { Calender } from "../components/Calender";
 import { useDispatch, useSelector } from "react-redux";
 import { closeModal, openModal } from "../reducers/ModalSlice";
 import { motion } from "motion/react";
